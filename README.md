e124e78fe98597c9721ec64985e8240ff33e6c1f
Ian Renton <ian@ianrenton.com>
2025-05-21 17:07:07 +0100

    Updated version

--- README.md
+++ README.md
@@ -1,9 +1,36 @@
 # UKBOTA n-fer finder
 
-Finds multiple bunker activation zone overlaps in the UKBOTA programme. Based on my ["Worked Everything, Everywhere, All at Once" script](https://github.com/ianrenton/weeaaoa), restricted to only a single UKBOTA bunkers CSV, but adds KML export for >=4fers.
+Finds multiple bunker activation zone overlaps in the UKBOTA programme. Based on my ["Worked Everything, Everywhere, All at Once" script](https://github.com/ianrenton/weeaaoa), restricted to only a single UKBOTA bunkers CSV, but adds KML export for >=3fers.
 
-4-fers are shown in blue in the KML output and 5-fers are shown in yellow. 
+3-fers are shown in green in the KML output, 4-fers in blue and 5-fers are shown in yellow. 
 
 Regions that are a 6-fer or more are shown in red - *please note that the UKBOTA rules do not allow activating more than 5 bunker references at a time*, so if you activate in these areas, you must pick your favourite 5.
 
-Note that the Public Domain licence applies only to the Python code. The UKBOTA bunker data is owned by the UKBOTA programme and subject to its own licence.
+![Screenshot](img/screenshot1.png) ![Screenshot](img/screenshot2.png)
+
+Note that the Public Domain licence applies only to the Python code. UKBOTA bunker data is owned by the UKBOTA programme and subject to its own licence.
+
+## Usage
+
+If you just want to see the current results as of May 2025, the `output.kml` that this script produces has been checked into the repo to avoid you having to re-run the script for yourself. Just [download that](https://github.com/ianrenton/ukbota-nfer-finder/raw/refs/heads/main/output.kml) and go.
+
+If you want to run the script for yourself with updated data, or to tweak things, read on.
+
+Usage with `venv` and `pip` on Linux is recommended, but it will work fine wherever you use Python and however you like to manage it.
+
+A complete setup example for Debian & derivatives such as Ubuntu:
+
+```bash
+sudo apt install python3 python3-pip python3-venv
+git clone https://github.com/ianrenton/ukbota-nfer-finder.git
+cd ukbota-nfer-finder
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+Then, download the latest UKBOTA CSV list from https://bunkersontheair.org/site/files/ to your computer. Edit `ukbota-nfer-finder.py` and set the value of the `BUNKERS_FILE_PATH` variable to point to the file you downloaded.
+
+You can then run the script with `python3 ukbota-nfer-finder.py`.
+
+Once it has finished, you can open the resulting `output.kml` in [Google Earth Pro](https://www.google.com/intl/en_uk/earth/about/versions/#download-pro) (desktop), [Google Earth](https://earth.google.com/web/) (web) or another KML viewer of your choice.
--- ukbota-nfer-finder.py
+++ ukbota-nfer-finder.py
@@ -1,8 +1,6 @@
 import csv
-import json
 import math
-import os.path
-import pathlib
+import os
 from datetime import datetime
 
 import geopandas as gpd
@@ -13,24 +11,17 @@ import simplekml
 from shapely import Polygon
 from shapely.geometry import Point, shape
 
-# File to use for raw data caching. If it exists, data will be loaded from it instead of querying the internet. To
-# force a redownload, just delete this file.
-DATA_FILE = ".cache/data.json"
-# File to use for geo data caching. If it exists, data will be loaded from it instead of running the processing. To
-# force re-running the processing, just delete this file.
-GEO_DATA_FILE = ".cache/geodata.json"
-# Files to output results to
-RESULT_FILE_JSON = "output.json"
+# UKBOTA CSV data file. Set this to point to your input data file.
+BUNKERS_FILE_PATH = "/home/ian/Downloads/UKBOTA-8_01-FINALcsv.csv"
+# File to output results to
 RESULT_FILE_KML = "output.kml"
 # To reduce output file size and processing time, ignore any regions that are overlaps, but of fewer than this number
 # of entities.
-MIN_OVERLAP_TO_REPORT = 4
+MIN_OVERLAP_TO_REPORT = 3
 # When converting a circle to a polygon, how many points should be used around the edge? Larger numbers mean longer
 # processing times, but more accurate results.
 CIRCLE_TO_POLY_POINTS = 128
-
-# UKBOTA CSV data file
-BUNKERS_FILE_PATH = "UKBOTA-8_01-FINALcsv.csv"
+# Activation radius of a bunker
 BUNKER_RADIUS_METRES = 1000
 
 # Static storage
@@ -39,36 +30,24 @@ OS_GRID_TO_WGS84_TRANSFORMER = pyproj.Transformer.from_crs(27700, 4326)
 
 # Data storage
 all_data = []
-data_file = pathlib.Path(DATA_FILE)
-geo_data_file = pathlib.Path(GEO_DATA_FILE)
-result_file = pathlib.Path(RESULT_FILE_JSON)
 
-# Main code starts here
-# If we don't have an existing data file, start fetching data from the internet.
-if not os.path.isfile(DATA_FILE):
-    # Retrieve bunkers data
-    print("Retrieving Bunker data...")
-    with open(BUNKERS_FILE_PATH, newline='', encoding="utf-8-sig") as csvfile:
-        reader = csv.DictReader(csvfile, dialect='excel')
-        for entity in reader:
-            all_data.append({"ref": entity["Description"],
-                             "name": entity["Name"],
-                             "type": "BUNKER",
-                             "radiusMetres": BUNKER_RADIUS_METRES,
-                             "lat": float(entity["Latitude"]),
-                             "lon": float(entity["Longitude"])})
+# Check source file exists
+if not os.path.isfile(BUNKERS_FILE_PATH):
+    print("Could not find " + BUNKERS_FILE_PATH + ". Check you have downloaded this data file and set the BUNKERS_FILE_PATH variable to point to it.")
+    exit(1)
 
-    # Write cache file
-    print("Writing cache file...")
-    data_file.parent.mkdir(exist_ok=True, parents=True)
-    data_file.write_text(json.dumps(all_data, indent=2))
-
-else:
-    print("Reading cache file...")
-    with open(data_file) as f:
-        all_data = json.load(f)
-
-print(str(len(all_data)) + " entities found.")
+# Load bunkers data
+print("Loading Bunker data...")
+with open(BUNKERS_FILE_PATH, newline='', encoding="utf-8-sig") as csvfile:
+    reader = csv.DictReader(csvfile, dialect='excel')
+    for entity in reader:
+        all_data.append({"ref": entity["Description"],
+                         "name": entity["Name"],
+                         "type": "BUNKER",
+                         "radiusMetres": BUNKER_RADIUS_METRES,
+                         "lat": float(entity["Latitude"]),
+                         "lon": float(entity["Longitude"])})
+print(str(len(all_data)) + " bunkers found.")
 
 print("Converting data for GeoPandas...")
 start = datetime.now()
@@ -89,49 +68,37 @@ for entity in all_data:
 
 all_buffers_geoseries = gpd.GeoSeries(list(map(lambda p: p["polygon"], all_data)))
 
-# Now run the processing, if we don't already have the result data
-if not os.path.isfile(geo_data_file):
-    # Assemble a GeoDataFrame containing all the entities.
-    data = {'name': list(map(lambda p: p["ref"] + " " + p["name"], all_data)),
-            'id': range(0, len(all_data)),
-            'geom': all_buffers_geoseries}
+# Assemble a GeoDataFrame containing all the entities.
+data = {'name': list(map(lambda p: p["ref"] + " " + p["name"], all_data)),
+        'id': range(0, len(all_data)),
+        'geom': all_buffers_geoseries}
 
-    df = pd.DataFrame(data, columns=['name', 'id', 'geom'])
-    gdf = gpd.GeoDataFrame(df, geometry='geom', crs=27700)
+df = pd.DataFrame(data, columns=['name', 'id', 'geom'])
+gdf = gpd.GeoDataFrame(df, geometry='geom', crs=27700)
 
-    print("Finding overlaps...")
-    # Code from https://gis.stackexchange.com/questions/387773/count-overlapping-features-using-geopandas
-    buffer_size = 0.1
-    bounds = gdf.geometry.convex_hull.exterior.buffer(buffer_size).unary_union
-    new_polys = list(shapely.ops.polygonize(bounds))
-    # Removing the full merged polygons (first is always index 0,
-    # subsequent will be the first of their own 'bunches' identified as disjoint from other 'bunches')
-    bad_poly_idx = [0]
-    while new_polys[max(bad_poly_idx)].disjoint(new_polys[-1]):
-        for idx in range(max(bad_poly_idx), len(new_polys)):
-            if new_polys[max(bad_poly_idx)].disjoint(new_polys[idx]):
-                bad_poly_idx += [idx]
-                break
-    new_polys = [new_polys[i].buffer(-buffer_size) for i in range(len(new_polys)) if i not in bad_poly_idx]
-    # count layers and track IDs of overlapping features
-    gdf_with_overlap_polys = gpd.GeoDataFrame(geometry=new_polys)
-    gdf_with_overlap_polys['layers'] = sum(
-        [gdf_with_overlap_polys.geometry.intersects(poly) for poly in gdf.geometry.buffer(buffer_size).values])
-    gdf_with_overlap_polys['piece'] = gdf_with_overlap_polys.index
+print("Finding overlaps...")
+# Code from https://gis.stackexchange.com/questions/387773/count-overlapping-features-using-geopandas
+buffer_size = 0.1
+bounds = gdf.geometry.convex_hull.exterior.buffer(buffer_size).unary_union
+new_polys = list(shapely.ops.polygonize(bounds))
+# Removing the full merged polygons (first is always index 0,
+# subsequent will be the first of their own 'bunches' identified as disjoint from other 'bunches')
+bad_poly_idx = [0]
+while new_polys[max(bad_poly_idx)].disjoint(new_polys[-1]):
+    for idx in range(max(bad_poly_idx), len(new_polys)):
+        if new_polys[max(bad_poly_idx)].disjoint(new_polys[idx]):
+            bad_poly_idx += [idx]
+            break
+new_polys = [new_polys[i].buffer(-buffer_size) for i in range(len(new_polys)) if i not in bad_poly_idx]
+# count layers and track IDs of overlapping features
+gdf_with_overlap_polys = gpd.GeoDataFrame(geometry=new_polys)
+gdf_with_overlap_polys['layers'] = sum(
+    [gdf_with_overlap_polys.geometry.intersects(poly) for poly in gdf.geometry.buffer(buffer_size).values])
+gdf_with_overlap_polys['piece'] = gdf_with_overlap_polys.index
 
-    runtime = datetime.now() - start
-    print("Generated " + str(len(gdf_with_overlap_polys.index)) + " overlap polys in " + str(
-        runtime.total_seconds()) + " seconds.")
-
-    print("Writing cache file...")
-    geo_data_file.parent.mkdir(exist_ok=True, parents=True)
-    geo_data_file.write_text(gdf_with_overlap_polys.to_json())
-    print("Written " + str(int(os.path.getsize(GEO_DATA_FILE) / 1024 / 1024)) + " MB.")
-
-else:
-    print("Reading cache file...")
-    with open(geo_data_file) as f:
-        gdf_with_overlap_polys = gpd.read_file(f)
+runtime = datetime.now() - start
+print("Generated " + str(len(gdf_with_overlap_polys.index)) + " overlap polys in " + str(
+    runtime.total_seconds()) + " seconds.")
 
 # Now iterate over the overlap poly features. Only care about ones with more than one "layer" (i.e. overlapping original
 # entity). For each such overlap poly, create a test point inside it, and see which original entities it's in range of.
@@ -163,10 +130,6 @@ print("Assessed " + str(len(gdf_with_overlap_polys.index)) + " overlap polys in 
 # Sort overlap data by number of entities
 overlap_data_sorted = sorted(overlap_data, key=lambda p: len(p["entities"]), reverse=True)
 
-print("Writing JSON results file...")
-result_file.parent.mkdir(exist_ok=True, parents=True)
-result_file.write_text(json.dumps(overlap_data_sorted, indent=2))
-
 print("Writing KML results file...")
 kml = simplekml.Kml()
 for d in overlap_data_sorted:
@@ -177,7 +140,10 @@ for d in overlap_data_sorted:
     centroid_point = shapely.centroid(Polygon(closed_poly))
     centroid_lonlat = shapely.get_coordinates(centroid_point).tolist()[0]
     poly = kml.newpolygon(outerboundaryis=closed_poly)
-    if len(d["entities"]) == 4:
+    if len(d["entities"]) == 3:
+        poly.style.polystyle.color = '9900ff00'
+        poly.style.linestyle.color = 'ff00ff00'
+    elif len(d["entities"]) == 4:
         poly.style.polystyle.color = '99ff0000'
         poly.style.linestyle.color = 'ffff0000'
     elif len(d["entities"]) == 5:
@@ -186,8 +152,11 @@ for d in overlap_data_sorted:
     else:
         poly.style.polystyle.color = '990000ff'
         poly.style.linestyle.color = 'ff0000ff'
+
     name = str(len(d["entities"])) + "-fer"
     description = "<br/>".join(d["entities"])
+    if len(d["entities"]) > 5:
+        description = description + "<br/><b>Please note that the UKBOTA rules do not allow activating more than 5 bunker references at a time. Pick your favourite 5 if activating here!</b>"
     kml.newpoint(name=name, description=description, coords=[(centroid_lonlat[0], centroid_lonlat[1])])
 kml.save(RESULT_FILE_KML)
 
